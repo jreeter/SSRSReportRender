3f0a1da80abff7211e62f23d892b60b8b2e6e32b
Joshua Reeter <joshreeter@gmail.com>
2016-08-17 17:45:42 -0500

    Add test to show usage.

---
+++ SSRSRenderTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using SSRSReporting;
+using System.Net;
+
+namespace SSRSRenderTest
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Report parameters defined in your .rdl files
+            Dictionary<string, string> reportParameters = new Dictionary<string, string>();
+            reportParameters.Add("reportParameter", "123");
+
+            // Execute the report 
+            SSRSReportClient rc = new SSRSReportClient("http://YOUR_SERVER_HERE/reportserver/reportexecution2005.asmx", "http://YOUR_SERVER_HERE/reportserver/reportservice2010.asmx", new NetworkCredential(@"user", "password"));
+            byte[] toolPickPDF = rc.ExecuteReport("PATH TO REPORT ON REPORTING SERVER", "PDF", reportParameters);
+        }
+    }
+}
